95c6d4d857372e65157c796927acbbce0cd43a93
Niv Monovich <niv313@gmail.com>
2021-10-08 13:04:43 +0300

    initial commit

---
+++ server/src/UserResolver.ts
@@ -0,0 +1,97 @@
+import {
+  Resolver,
+  Query,
+  Arg,
+  Ctx,
+  Mutation,
+  ObjectType,
+  Field,
+  UseMiddleware,
+  Int,
+} from 'type-graphql';
+import { hash, compare } from 'bcryptjs';
+import { User } from './entity/User';
+import { MyContext } from './MyContext';
+import { createAccessToken, createRefreshToken } from './auth';
+import { isAuth } from './isAuth';
+import { getConnection } from 'typeorm';
+import { sendRefreshToken } from './sendRefreshToken';
+
+@ObjectType()
+class LoginResponse {
+  @Field()
+  accessToken: string;
+}
+
+@Resolver()
+export class UserResolver {
+  @Query(() => String)
+  hello() {
+    return 'hi!';
+  }
+
+  @Query(() => String)
+  @UseMiddleware(isAuth)
+  bye(@Ctx() { payload }: MyContext) {
+    return `your user id is ${payload!.userId}`;
+  }
+
+  @Query(() => [User])
+  users() {
+    return User.find();
+  }
+
+  @Mutation(() => Boolean)
+  async register(
+    @Arg('email') email: string,
+    @Arg('password') password: string
+  ) {
+    const hashedPassword = await hash(password, 12);
+
+    try {
+      await User.insert({
+        email,
+        password: hashedPassword,
+      });
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+
+    return true;
+  }
+
+  @Mutation(() => Boolean)
+  async revokeRefreshTokensForUser(@Arg('userId', () => Int) userId: number) {
+    await getConnection()
+      .getRepository(User)
+      .increment({ id: userId }, 'tokenVersion', 1);
+
+    return true;
+  }
+
+  @Mutation(() => LoginResponse)
+  async login(
+    @Arg('email') email: string,
+    @Arg('password') password: string,
+    @Ctx() { res }: MyContext
+  ): Promise<LoginResponse> {
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      throw new Error("User wasn't found");
+    }
+
+    const valid = await compare(password, user.password);
+
+    if (!valid) {
+      throw new Error('Wrong password');
+    }
+
+    sendRefreshToken(res, createRefreshToken(user));
+
+    return {
+      accessToken: createAccessToken(user),
+    };
+  }
+}
